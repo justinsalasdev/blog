fc87405f699dce2683aa53460e91884a1004618d
l0ckstreet <rsalas7793@gmail.com>
2021-01-02 21:54:20 +0800

    fixed some grammar

--- components/Components/Components.js
+++ components/Components/Components.js
@@ -42,16 +42,16 @@ export default function Components_() {
 					<Description_prag expanded={one}>
 						We often want to invest to have either capital gains or to have some
 						amount of extra income - but how much return on our investments do
-						we really need? What are these sought after gains really for?
-						Investment planning is the core component of a financial plan
-						because it is geared towards the achievement of your financial
-						goals. This suggests that our investments must have a clear purpose
-						- but too often our financial goals are qualitative e.g "I want to
-						retire when I'm rich" or "Someday, I want to live in a big house".
-						My job as a financial planner is to quantify these qualitative goals
-						and craft an investment plan which take into account your goal
-						horizon, investable capital, adequate return on investment and the
-						amount of risk you're willing to take for your investments.{" "}
+						we need? What are these sought after gains really for? Investment
+						planning is the core component of a financial plan because it is
+						geared towards the achievement of your financial goals. This
+						suggests that our investments must have a clear purpose - but too
+						often our financial goals are qualitative e.g "I want to retire when
+						I'm rich" or "Someday, I want to live in a big house". My job as a
+						financial planner is to quantify these qualitative goals and craft
+						an investment plan which takes into account your goal horizon,
+						investable capital, adequate return on investment, and the amount of
+						risk you're willing to take for your investments.{" "}
 					</Description_prag>
 					<Description_action onClick={() => dispatch({ type: "one" })}>
 						{one ? "Show less" : "Read more"}
@@ -77,10 +77,10 @@ export default function Components_() {
 						aside for it? Allocating what would have been an investable amount
 						for minimizing risk is not outright tangible and satisfying. You'll
 						only appreciate your insurance the time that you'll be needing it -
-						and for sure, we don't want that time to come ever. Therefore, it is
-						important that we bring in the right balance between coverage
-						adequacy and practicality appropriate to your current financial
-						situation - insurance planning is a great tool for that.
+						and for sure, we don't want that time to come ever. Therefore, we
+						must bring in the right balance between coverage adequacy and
+						practicality appropriate to your current financial situation -
+						insurance planning is a great tool for that.
 					</Description_prag>
 					<Description_action onClick={() => dispatch({ type: "two" })}>
 						{two ? "Show less" : "Read more"}
@@ -95,9 +95,9 @@ export default function Components_() {
 						Tax planning isn't about tax avoidance. Taxation fuels our economy -
 						it serves as our contribution to our nation's progress. We can't
 						avoid our taxes - and we should not. Though the government is strict
-						on it's mandate, it didn't fail to consider giving legal exemptions
+						on its mandate, it didn't fail to consider giving legal exemptions
 						and tax deduction opportunities. By employing tax strategies
-						synonymous to your business structure, we can utilize these
+						synonymous with your business structure, we can utilize these
 						provisions to maximize income efficiency.{" "}
 					</Description_prag>
 					<Description_action onClick={() => dispatch({ type: "three" })}>
@@ -111,12 +111,12 @@ export default function Components_() {
 					<Title>Estate plan</Title>
 					<Description_prag expanded={four}>
 						Not a single day of our life is guaranteed. We are too busy building
-						our wealth that we forget to give little consideration of what
+						our wealth that we forget to give little consideration to what
 						happens to our estate when we're gone. The seasoned estate planner
 						Atty. Angelo M. Cabrera always says "He who deliberately leaves no
 						plan, plans chaos for his children". There's a lot of stories going
 						around about properties being eaten away by estate taxes and family
-						members suing each other on court for unsettled inheritance. These,
+						members suing each other in court for unsettled inheritance. These
 						and many other problems can be avoided with a proper estate plan.{" "}
 					</Description_prag>
 					<Description_action onClick={() => dispatch({ type: "four" })}>
